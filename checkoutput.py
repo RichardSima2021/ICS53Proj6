ff03bbc4dcb639ccfdc8a55dc858754ab66a7d21
Richard Sima <Richard.Sima2021@gmail.com>
2023-12-07 01:28:59 -0800

    some issue with threads not intertwining

---
+++ checkoutput.py
@@ -0,0 +1,70 @@
+import os
+
+def check_output_file(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    # Check the format of the first line
+    first_line = lines[0].strip().split()
+    if len(first_line) != 6 or first_line[0] != "./a.out":
+        print("Invalid format for the first line.")
+        return False
+
+    p, c, i, b, d = map(int, first_line[1:])
+    total_producers = p * i
+    total_consumers = c * i
+    produced_items = set()
+    consumed_items = set()
+    consumed_counts = {f'consumer_{j}': 0 for j in range(c)}
+
+    # Check the rest of the lines
+    for line in lines[1:]:
+        parts = line.strip().split()
+        if len(parts) != 4 or parts[1] not in ("produced", "consumed"):
+            print("Invalid format for line:", line)
+            return False
+
+        producer_consumer, action, item_str = parts[0], parts[1], parts[3]
+        item = int(item_str)
+
+        if producer_consumer.startswith("producer"):
+            if action == "produced":
+                if item in produced_items:
+                    print(f"Error: Producer {producer_consumer} produced item {item} more than once.")
+                    return False
+                produced_items.add(item)
+            else:
+                print(f"Error: Producer {producer_consumer} consumed item {item}.")
+                return False
+
+        elif producer_consumer.startswith("consumer"):
+            if action == "consumed":
+                consumed_counts[producer_consumer] += 1
+                if item in consumed_items:
+                    print(f"Error: Consumer {producer_consumer} consumed item {item} more than once.")
+                    return False
+                consumed_items.add(item)
+            else:
+                print(f"Error: Consumer {producer_consumer} produced item {item}.")
+                return False
+
+        else:
+            print("Invalid format for line:", line)
+            return False
+
+    # Check if all items are produced and consumed exactly once
+    if not (len(produced_items) == len(consumed_items) == total_producers):
+        print("Error: Some items were not produced or consumed.")
+        return False
+    
+    for consumer, count in consumed_counts.items():
+        if count != p*i/c:
+            print(f"Error: {consumer} consumed {count} items, expected {p*i/c}.")
+            return False
+
+    return True
+
+if check_output_file('output.txt'):
+    print("Successful")
+else:
+    print("Error")
--- hw6.c
+++ hw6.c
@@ -4,56 +4,107 @@
 #include <unistd.h>
 #include <pthread.h>
 
+// pointer to buffers
 int *buffer;
+// delay producer true?
 int delayPr;
+// length of buffer
 int buffLen;
+// num of items produced into the buffer
+int bufferProdCount;
+// num of items consumed from buffer
+int bufferConsumeCount;
+// num of items currently in the buffer
+int count;
+// how many items are produced by each producer
+int prodNum;
+// consumer limit
+int consumeLimit;
+
+
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; 
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t notFull = PTHREAD_COND_INITIALIZER;
+pthread_cond_t notEmpty = PTHREAD_COND_INITIALIZER;
 
 void *blewh(void *vargp) {
-    // printf("Hello from producer\n");
-    int i = * ((int*) vargp);
-    printf("I am producer %d\n", i);
-    int produceCount = 0;
+    int producerID = * ((int*) vargp);
+    for (int i = 0; i < prodNum; i++) {
+        // the item to be produced by producer_x on the ith iteration
+        int itemNum = producerID * prodNum + i;
+        pthread_mutex_lock(&mutex);
+        while (count == buffLen) {
+            // block and wait
+            pthread_cond_wait(&notFull, &mutex);
+        }
+        bufferProdCount++;
+        count ++;
+        int prodIndex = bufferProdCount % buffLen;
+        buffer[prodIndex] = itemNum;
+        printf("producer_%d produced item %d\n", producerID, itemNum);
+        if (delayPr) {
+            usleep(500000);
+        }
+        pthread_cond_signal(&notEmpty);
+        pthread_mutex_unlock(&mutex);
+    }
+
+    // exit(0);
 }
 
 void *nom(void *vargp) {
-    // printf("Hello from consumer\n");
-    int i = * ((int*) vargp);
-    printf("I am consumer %d\n", i);
-    int consumeCount = 0;
+    int consumerID = * ((int*) vargp);
+    for (int i = 0; i < consumeLimit; i++) {
+        pthread_mutex_lock(&mutex);
+        while (count == 0) {
+            // block and wait
+            pthread_cond_wait(&notEmpty, &mutex);
+        }
+        bufferConsumeCount++;
+        count--;
+        int consumeIndex = bufferConsumeCount % buffLen;
+        int consumedItem = buffer[consumeIndex];
+        printf("consumer_%d consumed item %d\n", consumerID, consumedItem);
+        if (!delayPr) {
+            usleep(500000);
+        }
+        
+        pthread_cond_signal(&notFull);
+        pthread_mutex_unlock(&mutex);
+    }
 
+    // exit(0);
 }
 
 
 int main(int argc, char **argv) {
     int numProd = atoi(argv[1]);
     int numCons = atoi(argv[2]);
-    int prodNum = atoi(argv[3]);
+    prodNum = atoi(argv[3]);
     buffLen = atoi(argv[4]);
     delayPr = atoi(argv[5]);
-    int consumeLimit = numProd * prodNum / numCons;
-
+    consumeLimit = numProd * prodNum / numCons;
+    bufferProdCount = 0;
+    bufferConsumeCount = 0;
+    count = 0;
     buffer = (int *)malloc(buffLen * sizeof(int));
 
     pthread_t producers[numProd];
     pthread_t consumers[numCons];
 
     for (int i = 0; i < numProd; i++) {
-        printf("Create producer_%d\n", i);
+        // printf("Create producer_%d\n", i);
         int *a = malloc(sizeof(int));
         *a = i;
         pthread_create(&producers[i], NULL, blewh, a);
     }
 
     for (int i = 0; i < numCons; i++) {
-        printf("Create consumer_%d\n", i);
+        // printf("Create consumer_%d\n", i);
         int *a = malloc(sizeof(int));
         *a = i;
         pthread_create(&consumers[i], NULL, nom, a);
     }
-
-    // printf("debug\n");
 
     for (int i = 0; i < numProd; i++) {
         pthread_join(producers[i], NULL);
